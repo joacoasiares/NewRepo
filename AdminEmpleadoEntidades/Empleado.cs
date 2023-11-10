7d6106a28274c3272fe24fe6d0bf7ff95301606c
user <user@DESKTOP>
2023-11-10 12:28:49 -0300

    Agregar archivos de proyecto.

---
+++ AdminEmpleadoEntidades/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminEmpleadoEntidades
+{
+    public class Empleado
+    {
+        public int id { get; set; }
+        public string? nombre { get; set; }
+        public String? dni { get; set; }
+        public string? direccion { get; set; }
+        public DateTime? fechaIngreso { get; set; }
+        public decimal? salario { get; set; }
+        public Departamento? Departamento { get; set; }
+        public String? nombreDepartamento
+        {
+            get
+            {
+                if (Departamento != null)
+                {
+                    return nombreDepartamento;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ AdminEmpleadoNegocio/EmpleadosNegocio.cs
@@ -0,0 +1,18 @@
+﻿using AdminEmpleadoEntidades;
+using AdminEmpleadosDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpleadosNegocio
+{
+    public class EmpladoNegocio
+    {
+        public static List<Empleado> GET(Empleado e)
+        {
+            return EmpleadosDatos.Get(e);
+        }
+    }
+}
